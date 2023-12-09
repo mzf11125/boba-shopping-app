9f092a5487a5019a56d2cfc3c45f876d39afe832
mzf11125 <mzidanfatonie@gmail.com>
2023-12-10 06:20:53 +0700

    fixed stuff

--- lib/login.dart
+++ lib/login.dart
@@ -66,7 +66,7 @@ class _LoginPageState extends State<LoginPage> {
                             }
                             return null;
                           },
-                        ),
+                        ), //Textformfield
                         SizedBox(
                           height: 30,
                         ),
